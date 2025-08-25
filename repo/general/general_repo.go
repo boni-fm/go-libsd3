a5600bd398dfc62a6ec50f8bd58b0942aa64215f
BonifasiusM <bonifasiusm@indomaret.co.id>
2025-08-25 10:42:17 +0700

    feat: add general repo queries

--- repo/general/general_repo.go
+++ repo/general/general_repo.go
@@ -1,10 +1,14 @@
 package general
 
 import (
+	"fmt"
+
+	"github.com/boni-fm/go-libsd3/helper/logging"
 	"github.com/boni-fm/go-libsd3/pkg/dbutil"
 )
 
-func GetUser() {
+func GetUser(username string) {
+	log := logging.NewLogger()
 	db, err := dbutil.Connect()
 	if err != nil {
 		return
@@ -12,4 +16,19 @@ func GetUser() {
 	defer db.Close()
 
 	// Implement your user retrieval logic here
+	rows, err := db.Select("Select user_password from dc_user_t where user_name = $1", username)
+	if err != nil {
+		log.SayError(err.Error())
+		return
+	}
+	defer rows.Close()
+
+	var pass string
+	if rows.Next() {
+		if err := rows.Scan(&pass); err != nil {
+			log.SayError(err.Error())
+			return
+		}
+	}
+	fmt.Println(pass)
 }
